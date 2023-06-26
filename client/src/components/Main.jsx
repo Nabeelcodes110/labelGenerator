117f56fb19b2ec10a21d3338fc2d7edccc29d0f5
omkar walve <anuragwalve@gmail.com>
2023-06-27 02:59:09 +0530

    added login and dashboard pages

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-
+import React, { useState } from 'react'
+import Login from './Login'
+import Dashboard from './Dashboard'
 
 const Main = ()=>{
-    return (
-        <div>
-            Main
-        </div>
-    )
+    let [profile,setprofile]=useState(false)
+    
+    return(<div className="content-bg">{profile===false? (<Login setprofile={setprofile}/>):(<Dashboard />)}</div>)
+        
 }
 
 export default Main;
