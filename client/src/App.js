351a5a3b0baa566e94482daf93c33d2cac6fd2fb
omkar walve <anuragwalve@gmail.com>
2023-06-27 00:28:29 +0530

    added client

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import Main from "./components/Main";
+
+const App = () => {
+    return (
+     
+      <div>
+       <BrowserRouter>
+       <Main />      
+        </BrowserRouter>
+      </div>
+      
+    );
+   };
+    
+   export default App;
